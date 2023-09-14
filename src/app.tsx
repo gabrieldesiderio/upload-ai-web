b62dcadfd36ce6cd83ea9a5fe7f0d6770a82b547
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-09-14 13:11:12 -0300

    feat: create videoId status

--- src/app.tsx
+++ src/app.tsx
@@ -7,8 +7,16 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '.
 import { Slider } from './components/ui/slider'
 import { VideoInputForm } from './components/video-input-form'
 import { PromptSelect } from './components/prompt-select'
+import { useState } from 'react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  function handlePromptSelected(template: string) {
+    console.log(template)
+  }
+
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='px-6 py-3 flex items-center justify-between border-b'>
@@ -48,14 +56,14 @@ export function App() {
           </p>
         </div>
         <aside className='w-80 space-y-6'>
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
           <form className='space-y-6'>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <PromptSelect />
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
 
             <div className='space-y-2'>
@@ -81,6 +89,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className='block text-sm text-muted-foreground italic leading-relaxed'>
                 Valores mais altos tendem a deixar o resultado mais criativo e com possível erros.
