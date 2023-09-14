91fb357c334e27d756aea48c166594a2c36410e5
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-09-14 11:55:26 -0300

    feat: get prompt value with ref

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,10 +3,11 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -20,6 +21,16 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
+  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if(!videoFile) {
+      return
+    }
+  }
+
   const previewURL = useMemo(() => {
     if (!videoFile) {
       return null
@@ -29,7 +40,7 @@ export function VideoInputForm() {
   }, [videoFile])
 
   return (
-    <form className='space-y-6'>
+    <form onSubmit={handleUploadVideo} className='space-y-6'>
       <label 
         htmlFor="video"
         className='relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
@@ -56,6 +67,7 @@ export function VideoInputForm() {
       <div className='space-y-2'>
         <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
         <Textarea 
+          ref={promptInputRef}
           id="transcription_prompt" 
           className='h-20 leading-relaxed resize-none'
           placeholder='Inclua palavras-chame mencionadas no vídeo separadar por vírgula (,)'
